5b28131751f5a9f17a1eafe06ebf802970fbe068
Andre Arko <andre@arko.net>
2018-08-26 21:03:08 -0500

    handle both plain and gzipped log files

--- src/main.rs
+++ src/main.rs
@@ -46,19 +46,26 @@ struct Request {
 use flate2::read::GzDecoder;
 use std::env;
 use std::error::Error;
+use std::ffi::OsStr;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::path::Path;
 
-fn read_file(path: &str) -> BufReader<GzDecoder<File>> {
-  let path = Path::new(path);
+fn read_file(filename: &str) -> Box<BufRead> {
+  let path = Path::new(filename);
   let file = match File::open(&path) {
     Err(why) => panic!("couldn't open {}: {}", path.display(), why.description()),
     Ok(file) => file,
   };
 
-  BufReader::new(GzDecoder::new(file))
+  println!("{:?}", path.extension());
+
+  if path.extension() == Some(OsStr::new("gz")) {
+    Box::new(BufReader::new(GzDecoder::new(file)))
+  } else {
+    Box::new(BufReader::new(file))
+  }
 }
 
 fn main() {

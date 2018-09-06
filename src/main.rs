f396d43d04245ba4d6ce6793959caeeef64e86fb
Andre Arko <andre@arko.net>
2018-09-05 23:45:35 -0700

    collect opts, accept multiple file args

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ extern crate serde_json;
 extern crate test;
 extern crate time;
 
-use argparse::{ArgumentParser, Store, StoreTrue};
+use argparse::{ArgumentParser, Collect, StoreTrue};
 use std::collections::HashMap;
 use std::io::*;
 
@@ -22,36 +22,19 @@ mod file;
 mod request;
 mod user_agent;
 
-fn main() {
-  let mut verbose = false;
-  let mut path = "test/sample_10.log.gz".to_string();
-  let mut show_misses = false;
+type StatsMap = HashMap<String, HashMap<String, HashMap<String, i32>>>;
 
-  {
-    let mut ap = ArgumentParser::new();
-    ap.set_description("Parse a RubyGems.org Fastly JSON log file.");
-    ap.refer(&mut show_misses).add_option(
-      &["--misses"],
-      StoreTrue,
-      "Print only user agents strings that failed to parse",
-    );
-    ap.refer(&mut verbose)
-      .add_option(&["-v", "--verbose"], StoreTrue, "Be verbose");
-    ap.refer(&mut path)
-      .add_argument("FILE", Store, "Path of the log file to parse");
-    ap.parse_args_or_exit();
-  }
-
+fn file_to_stats(path: &str, opts: &Options) -> StatsMap {
   let mut lineno = 0;
   let mut times = HashMap::new();
 
-  if verbose {
+  if opts.verbose {
     println!("Opening log file {}", path);
   }
   let file = file::reader(&path);
 
   for line in file.lines() {
-    if verbose {
+    if opts.verbose {
       lineno += 1;
       if lineno % 100_000 == 0 {
         print!(".");
@@ -64,7 +47,7 @@ fn main() {
     let hour = [r.timestamp.get(..14).unwrap(), "00:00"].concat();
     let mut counters = times.entry(hour).or_insert(HashMap::new());
 
-    if show_misses {
+    if opts.show_misses {
       match user_agent::parse(&r.user_agent) {
         None => println!("{}", r.user_agent),
         Some(_) => (),
@@ -74,24 +57,66 @@ fn main() {
 
     if let Some(ua) = user_agent::parse(&r.user_agent) {
       {
-        let rubygems = counters.entry("rubygems").or_insert(HashMap::new());
+        let rubygems = counters
+          .entry("rubygems".to_string())
+          .or_insert(HashMap::new());
         let count = rubygems.entry(String::from(ua.rubygems)).or_insert(0);
         *count += 1;
       }
 
       if let Some(bundler_version) = ua.bundler {
-        let bundlers = counters.entry("bundler").or_insert(HashMap::new());
+        let bundlers = counters
+          .entry("bundler".to_string())
+          .or_insert(HashMap::new());
         let count = bundlers.entry(String::from(bundler_version)).or_insert(0);
         *count += 1;
       }
 
       if let Some(ruby_version) = ua.ruby {
-        let rubies = counters.entry("ruby").or_insert(HashMap::new());
+        let rubies = counters.entry("ruby".to_string()).or_insert(HashMap::new());
         let count = rubies.entry(String::from(ruby_version)).or_insert(0);
         *count += 1;
       }
     }
   }
-  println!("");
-  println!("{:?}", times);
+
+  if opts.verbose {
+    println!("");
+  }
+
+  times
+}
+
+struct Options {
+  verbose: bool,
+  show_misses: bool,
+  paths: Vec<String>,
+}
+
+fn main() {
+  let mut opts = Options {
+    paths: ["test/sample_10.log.gz".to_string()].to_vec(),
+    show_misses: false,
+    verbose: false,
+  };
+
+  {
+    let mut ap = ArgumentParser::new();
+    ap.set_description("Parse a RubyGems.org Fastly JSON log file.");
+    ap.refer(&mut opts.show_misses).add_option(
+      &["--misses"],
+      StoreTrue,
+      "Print only user agents strings that failed to parse",
+    );
+    ap.refer(&mut opts.verbose)
+      .add_option(&["-v", "--verbose"], StoreTrue, "Be verbose");
+    ap.refer(&mut opts.paths)
+      .add_argument("FILE", Collect, "Path of the log file to parse");
+    ap.parse_args_or_exit();
+  }
+
+  for path in &opts.paths {
+    let times = file_to_stats(&path, &opts);
+    println!("{:?}", times);
+  }
 }

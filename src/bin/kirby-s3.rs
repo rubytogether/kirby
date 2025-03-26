883f9bbf5fa7ab48f3af593d9df573ae7cd6ace7
Samuel Giddins <segiddins@segiddins.me>
2025-03-25 21:55:14 -0700

    Migrate from rusoto to the aws sdk
    Since rusoto is deprecated
    
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>

--- src/bin/kirby-s3.rs
+++ src/bin/kirby-s3.rs
@@ -3,43 +3,38 @@ extern crate serde_json;
 
 extern crate kirby;
 
+use aws_config::meta::region::RegionProviderChain;
 use aws_lambda_events::event::s3::S3Event;
 use aws_lambda_events::sns::SnsEventObj;
+use aws_sdk_s3::Client;
+use aws_sdk_s3::operation::put_object::PutObjectOutput;
+use aws_sdk_s3::primitives::ByteStream;
 use flate2::read::GzDecoder;
 use lambda_runtime::tracing::{self, info, warn};
 use lambda_runtime::{Error, LambdaEvent, service_fn};
 use percent_encoding::percent_decode;
-use rusoto_core::region::Region;
-use rusoto_s3::*;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Cursor;
-use tokio::io::AsyncReadExt;
 
 use kirby::Options;
 use kirby::stream_stats;
 
-async fn read_object(bucket_name: &str, key: &str) -> Box<dyn BufRead> {
-    let get_req = GetObjectRequest {
-        bucket: bucket_name.to_owned(),
-        key: key.to_owned(),
-        ..Default::default()
-    };
-
-    let client = S3Client::new(Region::UsWest2);
-    let result = client
-        .get_object(get_req)
+async fn read_object(client: &Client, bucket_name: &str, key: &str) -> Box<dyn BufRead> {
+    let object = client
+        .get_object()
+        .bucket(bucket_name)
+        .key(key)
+        .send()
         .await
         .expect("Couldn't GET object");
 
-    let mut bytes = Vec::new();
-    result
+    let bytes = object
         .body
-        .unwrap()
-        .into_async_read()
-        .read_to_end(&mut bytes)
+        .collect()
         .await
-        .expect("Couldn't read object body stream");
+        .expect("Couldn't collect object body stream")
+        .into_bytes();
 
     if key.ends_with("gz") {
         Box::new(BufReader::new(GzDecoder::new(Cursor::new(bytes))))
@@ -48,19 +43,25 @@ async fn read_object(bucket_name: &str, key: &str) -> Box<dyn BufRead> {
     }
 }
 
-async fn write_object(bucket_name: &str, key: &str, body: &str) -> rusoto_s3::PutObjectOutput {
-    let req = PutObjectRequest {
-        bucket: bucket_name.to_owned(),
-        key: key.to_owned(),
-        body: Some(StreamingBody::from(body.as_bytes().to_vec())),
-        ..Default::default()
-    };
-
-    let client = S3Client::new(Region::UsWest2);
-    client.put_object(req).await.expect("Couldn't PUT object")
+async fn write_object<B>(client: &Client, bucket_name: &str, key: &str, body: B) -> PutObjectOutput
+where
+    B: Into<ByteStream>,
+{
+    client
+        .put_object()
+        .bucket(bucket_name)
+        .key(key)
+        .body(body.into())
+        .send()
+        .await
+        .expect("Couldn't PUT object")
 }
 
 async fn func(event: LambdaEvent<SnsEventObj<S3Event>>) -> Result<(), Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&config);
+
     let opts = Options {
         paths: vec![],
         verbose: false,
@@ -84,7 +85,7 @@ async fn func(event: LambdaEvent<SnsEventObj<S3Event>>) -> Result<(), Error> {
                 bucket_name,
                 &key
             );
-            let reader = read_object(bucket_name, &key).await;
+            let reader = read_object(&client, bucket_name, &key).await;
 
             info!("{} calculating stats...", time::now_utc().rfc3339());
             let content = stream_stats(reader, &opts);
@@ -97,7 +98,13 @@ async fn func(event: LambdaEvent<SnsEventObj<S3Event>>) -> Result<(), Error> {
                 time::now_utc().rfc3339(),
                 &result_key
             );
-            write_object(bucket_name, &result_key, &json!(content).to_string()).await;
+            write_object(
+                &client,
+                bucket_name,
+                &result_key,
+                json!(content).to_string().into_bytes(),
+            )
+            .await;
 
             info!("{} done with {}", time::now_utc().rfc3339(), &key);
         }
